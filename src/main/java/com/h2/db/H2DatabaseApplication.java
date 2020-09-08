6c94ef7556436de23bef66e1ca9a31d6361fecdf
tahsin <amin1996bangladesh>
2020-09-08 11:26:50 -0400

    How to bring list of names?

--- src/main/java/com/h2/db/H2DatabaseApplication.java
+++ src/main/java/com/h2/db/H2DatabaseApplication.java
@@ -8,6 +8,7 @@ public class H2DatabaseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(H2DatabaseApplication.class, args);
+		System.out.println("hello");
 	}
 
 }
